6245a30788313ddc261e5eb4f606e73e1f575e5d
trxe <peixianwong@gmail.com>
2021-09-18 11:15:51 +0800

    Complete Tutorial RemarkCommand

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -31,7 +31,8 @@ public class Remark {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidRemark(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return true;
+        // return test.matches(VALIDATION_REGEX);
     }
 
     @Override
